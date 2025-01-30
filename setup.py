2df36e8573ddb77bf4aa6af9614cb02cbf306cc8
mane <mane@manesec.com>
2025-01-30 13:41:22 +0800

    init

--- setup.py
+++ setup.py
@@ -8,25 +8,39 @@ def install_requirements():
     all_requirements = []
 
     all_requirements.extend(read_requirements('src/pyGPOAbuse/requirements.txt'))
+    all_requirements.extend(read_requirements('src/GPOwned/requirements.txt'))
+    all_requirements.extend(read_requirements('src/wmiexec-Pro/requirements.txt'))
+    all_requirements.extend(read_requirements('src/windapsearch/requirements.txt'))
 
     return all_requirements
 
 setup(
-    name='pipx4tools', 
+    name='pipx4tools',
     version='0.0.2',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'pygpoabuse.py=src.start:start_pygpoabuse', 
+            'pygpoabuse.py=src.start:start_pygpoabuse',
+            'GPOwned.py=src.start:start_GPOwned',
+            'addspn.py=src.start:start_addspn',
+            'dnstool.py=src.start:start_dnstool',
+            'krbrelayx.py=src.start:start_krbrelayx',
+            'printerbug.py=src.start:start_printerbug',
+            'noPac.py=src.start:start_nopac',
+            'nopac_scanner.py=src.start:start_nopac_scanner',
+            'wmiexec-pro.py=src.start:start_wmiexec_pro',
+            'windapsearch.py=src.start:start_windapsearch',
+            'PetitPotam.py=src.start:start_petitpotam',
         ],
     },
     install_requires=install_requirements(),
-    python_requires='>=3.6', 
-    author='manesec', 
-    author_email='mane@manesec.com',  
+    python_requires='>=3.6',
+    author='manesec',
+    author_email='mane@manesec.com',
     description='collection for pipx with redteam tools.',
-    long_description=open('README.md').read(),  
-    long_description_content_type='text/markdown', 
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/manesec/spose-thread',
     classifiers=[
         'Programming Language :: Python :: 3',
--- src/start.py
+++ src/start.py
@@ -1,6 +1,82 @@
-import os
+import os,subprocess,sys
+from pathlib import Path
 
-# start and run pygpoabuse
+# pygpoabuse
 def start_pygpoabuse():
-    os.chdir('src/pyGPOAbuse')
-    exec(open('pygpoabuse.py').read())
+    script_path = Path(__file__).resolve().parent / "pyGPOAbuse" / "pygpoabuse.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command) 
+
+
+# GPOwned
+def start_GPOwned():
+    script_path = Path(__file__).resolve().parent / "GPOwned" / "GPOwned.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+
+# krbrelayx : addspn
+def start_addspn():
+    script_path = Path(__file__).resolve().parent / "krbrelayx" / "addspn.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# krbrelayx : dnstool
+def start_dnstool():
+    script_path = Path(__file__).resolve().parent / "krbrelayx" / "dnstool.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# krbrelayx : printerbug
+def start_printerbug():
+    script_path = Path(__file__).resolve().parent / "krbrelayx" / "printerbug.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# krbrelayx : krbrelayx
+def start_krbrelayx():
+    script_path = Path(__file__).resolve().parent / "krbrelayx" / "krbrelayx.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+
+# noPac : noPac
+def start_nopac():
+    script_path = Path(__file__).resolve().parent / "noPac" / "noPac.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# noPac : scanner
+def start_nopac_scanner():
+    script_path = Path(__file__).resolve().parent / "noPac" / "scanner.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# wmiexec-pro
+def start_wmiexec_pro():
+    script_path = Path(__file__).resolve().parent / "wmiexec-Pro" / "wmiexec-pro.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# windapsearch
+def start_windapsearch():
+    script_path = Path(__file__).resolve().parent / "windapsearch" / "windapsearch.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
+
+# PetitPotam
+def start_petitpotam():
+    script_path = Path(__file__).resolve().parent / "PetitPotam" / "PetitPotam.py"
+    args = sys.argv[1:]
+    command = [sys.executable, str(script_path)] + args
+    subprocess.run(command)
